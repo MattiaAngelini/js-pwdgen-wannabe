4db156f1a07d77204126a4f7124fb20b515ee530
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-04 14:44:36 +0100

    Fixed: removed console.log

--- scripts.js
+++ scripts.js
@@ -3,15 +3,12 @@
 
 // Ci serve: nome dell'utente
 const userName = prompt('Ciao dimmi il tuo nome');
-console.log(userName);
 
 // Ci serve: cognome dell'utente
 const userSurname = prompt('Ciao dimmi il tuo cognome');
-console.log(userSurname);
 
 // Ci serve: il colore preferito dell'utente
 const userPreferitColor = prompt('Ciao dimmi il tuo colore preferito');
-console.log(userPreferitColor);
 
 // Scriviamo nome cognome colore preferito e 23 nel documento.
 document.getElementById('IntroMessage').innerHTML = userName + userSurname + userPreferitColor + 23;
